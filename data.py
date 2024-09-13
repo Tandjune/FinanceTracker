29e55b225c774b9691e0f6c07d925cebbcf5b280
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-09-13 20:51:23 +0200

    create csv file

---
+++ data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import csv
+from datetime import datetime as dt
+
+
+
+
+class CSV:
+    csv_file = "finance_data.csv"
+    columns = ["date", "amount", "category", "description"]
+
+    @classmethod
+    def create_file(self):
+        try:
+            pd.read_csv(self.csv_file)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=self.columns)
+            df.to_csv(self.csv_file, index=False)
+
+    @classmethod
+    def add_entry(self, date, amount, category, description):
+        new = {
+            "date":date,
+            "amount":amount,
+            "category":category,
+            "description":description
+        }
+        with open(self.csv_file, "a", newline="") as csvFile:
+            writer = csv.DictWriter(csvFile, fieldnames=self.columns)
+            writer.writerow(new)
+        print("New entry added successfully")
+
+
+
+CSV.create_file()
